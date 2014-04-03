7b0982b89505893b645d7bd285ab10da1942bb8f
fred mac <mcfredmac@icloud.com>
2014-04-03 13:58:25 +0100

    sorted file structure

--- server.rb
+++
@@ -1,7 +0,0 @@
-require 'json'
-require 'net/http'
-
-uri = URI('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=bbcradio1&api_key=dc9fd34e42449b66f86e39aa8b77f230&format=json')
-response = Net::HTTP.get(uri)
-
-ruby_hash = JSON.parse(response)
